729fbefdb532fc30c082221b9f6cfed6c5e1dcac
heliopietsu-estacio <heliopietroski@gmail.com>
2026-07-27 12:22:33 +0000

    Inclusão do código completo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,144 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+// Tema 2 - Comparação das Cartas (Nível Aventureiro)
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    // Variáveis para a Carta 1
+    char nomeA[50];
+    int populacaoA, turisticosA;
+    float areaA, pibA, densidadeA;
 
+    // Variáveis para a Carta 2
+    char nomeB[50];
+    int populacaoB, turisticosB;
+    float areaB, pibB, densidadeB;
+
+    // --- Cadastro das Cartas ---
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+    printf("=== Cadastro da Carta 1 ===\n");
+    printf("Nome do pais: ");
+    scanf(" %49[^\n]", nomeA); // Lê a string com espaços
+    printf("Populacao: ");
+    scanf("%d", &populacaoA);
+    printf("Area (km2): ");
+    scanf("%f", &areaA);
+    printf("PIB: ");
+    scanf("%f", &pibA);
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &turisticosA);
+    
+    // Cálculo da densidade demográfica (Carta 1)
+    densidadeA = populacaoA / areaA;
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    printf("\n=== Cadastro da Carta 2 ===\n");
+    printf("Nome do pais: ");
+    scanf(" %49[^\n]", nomeB);
+    printf("Populacao: ");
+    scanf("%d", &populacaoB);
+    printf("Area (km2): ");
+    scanf("%f", &areaB);
+    printf("PIB: ");
+    scanf("%f", &pibB);
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &turisticosB);
+    
+    // Cálculo da densidade demográfica (Carta 2)
+    densidadeB = populacaoB / areaB;
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+    // --- Menu Interativo ---
+    int opcao;
+    printf("\n==================================\n");
+    printf("   MENU DE COMBATE - SUPER TRUNFO   \n");
+    printf("==================================\n");
+    printf("Escolha o atributo para comparar:\n");
+    printf("1. Populacao\n");
+    printf("2. Area\n");
+    printf("3. PIB\n");
+    printf("4. Pontos Turisticos\n");
+    printf("5. Densidade Demografica (Menor vence!)\n");
+    printf("Opcao: ");
+    scanf("%d", &opcao);
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    printf("\n--- RESULTADO DA BATALHA ---\n");
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    // --- Comparação de Cartas com Switch e If-Else ---
+    switch (opcao) {
+        case 1: // POPULAÇÃO
+            printf("Atributo: Populacao\n");
+            printf("%s: %d\n", nomeA, populacaoA);
+            printf("%s: %d\n", nomeB, populacaoB);
+            
+            if (populacaoA > populacaoB) {
+                printf("Vencedor: %s!\n", nomeA);
+            } else if (populacaoB > populacaoA) {
+                printf("Vencedor: %s!\n", nomeB);
+            } else {
+                printf("Resultado: Empate!\n");
+            }
+            break;
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+        case 2: // ÁREA
+            printf("Atributo: Area\n");
+            printf("%s: %.2f km2\n", nomeA, areaA);
+            printf("%s: %.2f km2\n", nomeB, areaB);
+            
+            if (areaA > areaB) {
+                printf("Vencedor: %s!\n", nomeA);
+            } else if (areaB > areaA) {
+                printf("Vencedor: %s!\n", nomeB);
+            } else {
+                printf("Resultado: Empate!\n");
+            }
+            break;
+
+        case 3: // PIB
+            printf("Atributo: PIB\n");
+            printf("%s: %.2f\n", nomeA, pibA);
+            printf("%s: %.2f\n", nomeB, pibB);
+            
+            if (pibA > pibB) {
+                printf("Vencedor: %s!\n", nomeA);
+            } else if (pibB > pibA) {
+                printf("Vencedor: %s!\n", nomeB);
+            } else {
+                printf("Resultado: Empate!\n");
+            }
+            break;
+
+        case 4: // PONTOS TURÍSTICOS
+            printf("Atributo: Pontos Turisticos\n");
+            printf("%s: %d\n", nomeA, turisticosA);
+            printf("%s: %d\n", nomeB, turisticosB);
+            
+            if (turisticosA > turisticosB) {
+                printf("Vencedor: %s!\n", nomeA);
+            } else if (turisticosB > turisticosA) {
+                printf("Vencedor: %s!\n", nomeB);
+            } else {
+                printf("Resultado: Empate!\n");
+            }
+            break;
+
+        case 5: // DENSIDADE DEMOGRÁFICA (REGRA INVERTIDA)
+            printf("Atributo: Densidade Demografica\n");
+            printf("%s: %.2f hab/km2\n", nomeA, densidadeA);
+            printf("%s: %.2f hab/km2\n", nomeB, densidadeB);
+            
+            // Note que aqui o sinal é de MENOR (<), pois a menor densidade vence
+            if (densidadeA < densidadeB) {
+                printf("Vencedor: %s!\n", nomeA);
+            } else if (densidadeB < densidadeA) {
+                printf("Vencedor: %s!\n", nomeB);
+            } else {
+                printf("Resultado: Empate!\n");
+            }
+            break;
+
+        default: // TRATAMENTO DE ERRO (Opção inválida)
+            printf("Erro: Opcao invalida! Escolha um numero de 1 a 5.\n");
+            break;
+    }
 
     return 0;
 }
